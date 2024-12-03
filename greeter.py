686cb8f66db78fea23f7cf0db6480ccad91cd3ca
alexb <alex.brown577@gmail.com>
2024-12-03 13:02:01 -0500

    1.2

---
+++ greeter.py
@@ -0,0 +1,8 @@
+greets = ["hi",
+          "hello",
+          "howdy",
+          "good day"
+          ]
+
+for greeting in greets:
+    print(f"{greeting}!")
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-greets = ["hi",
-          "hello",
-          "howdy",
-          "good day",
-          "nihao"]
-
-for greeting in greets:
-    print(f"{greeting}!")
+# greets = ["hi",
+#           "hello",
+#           "howdy",
+#           "good day",
+#           "nihao"]
+#
+# for greeting in greets:
+#     print(f"{greeting}!")
